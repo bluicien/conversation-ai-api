9cee10cb7f302e89545869823cb074d68224c5d8
Brendon <bluicien@yahoo.com>
2025-07-10 22:59:26 +1200

    Updated error handling of chat controller

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -11,8 +11,19 @@ export const chatWithGemini = async (req: Request, res: Response) => {
 
     try {
         const aiResponse = await sendGeminiMessage(history);
+        if (typeof aiResponse !== "object" || aiResponse === null || !("reply" in aiResponse) || !("newChatHistory" in aiResponse)) {
+            res.status(500).json({ message: "Unexpected response format from AI." });
+            return;
+        }
+
+        if (!aiResponse.reply || !Array.isArray(aiResponse.newChatHistory) || aiResponse.newChatHistory.length === 0) {
+            res.status(404).json({ message: "Failed to get response from AI."});
+            return;
+        }
+
         res.status(200).json(aiResponse);
         return 
+
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Internal Server Error" });
