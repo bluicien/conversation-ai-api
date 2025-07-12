88f3335a50d96066cd8bef3275df122ba1af8c54
Brendon <bluicien@yahoo.com>
2025-07-12 22:11:16 +1200

    Added cors options for client URL only

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    methods: ['GET', 'POST']
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 startKnowledgeBase();
